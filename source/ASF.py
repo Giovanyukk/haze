ad5100f828ad081ccb54acd82645599b471a6c09
Chen <enzo.sanchez.croce@alu.ing.unlp.edu.ar>
2021-07-06 02:09:10 -0300

    Error handling - idle_bot

--- source/ASF.py
+++ source/ASF.py
@@ -9,10 +9,12 @@ URL = 'http://127.0.0.1:1242'
 
 
 def log(data: str):
+    '''Imprimir datos con estampa de tiempo'''
     print('[' + datetime.now().strftime('%d/%m/%y %H:%M') + '] ' + data)
 
 
 def get(endpoint: str):
+    '''Enviar solicitud GET a ASF'''
     headers = {'Authentication': '', 'accept': 'application/json'}
     uri = f'{URL}/Api/{endpoint}'
     try:
@@ -22,6 +24,7 @@ def get(endpoint: str):
 
 
 def post(endpoint: str, data: str):
+    '''Enviar solicitud POST a ASF'''
     headers = {'Authentication': '', 'accept': 'application/json',
                'Content-Type': 'application/json'}
     uri = f'{URL}/Api/{endpoint}'
@@ -29,11 +32,15 @@ def post(endpoint: str, data: str):
 
 
 def cmd(data: str):
+    '''Enviar un comando a ASF'''
     #log(f'CMD: {data}')
     post('Command', '{"Command":"' + data + '"}')
 
 
 def idle_bot(bot: str, ismain=False):
+    '''Iniciar el algoritmo fast-mode para un bot
+    Esta función esta diseñada para ser utilizada en paralelo al codigo principal con el modulo threading
+    '''
     sleep(10)
 
     logged_on = get(f'Bot/{bot}')
@@ -43,64 +50,70 @@ def idle_bot(bot: str, ismain=False):
         logged_on = get(f'Bot/{bot}')
         retries += 1
         if(retries > 3):
-            log(f'No se ha podido conectar el bot {bot}')
+            log(f'{bot} - No se ha podido establecer la conexión con ASF')
             return 1
-    with open('test.json', 'w') as f:
-        json.dump(logged_on.json(), f)
 
-    log(f'Bot {bot} conectado correctamente')
+    log(f'{bot} - Conexión exitosa')
 
     cmd(f'pause {bot}')
     cmd(f'start {bot}')
 
-    while(True):
-        log(f'Buscando juegos con cromos restantes para {bot}...')
-        cmd(f'resume {bot}')
-        sleep(10)
-        cmd(f'pause {bot}')
+    try:
+        while(True):
+            try:
+                cmd(f'resume {bot}')
+            except:
+                log(f'No se ha encontrado un bot con el nombre {bot}')
+                break
+            log(f'{bot} - Buscando juegos con cromos restantes...')
+            sleep(10)
+            cmd(f'pause {bot}')
 
-        games_to_farm = get(
-            f'Bot/{bot}').json()['Result'][bot]['CardsFarmer']['GamesToFarm']
-        hours_played = max([games_to_farm[i]['HoursPlayed']
-                           for i in range(len(games_to_farm))], default=0)
-        remaining_cards = sum([int(games_to_farm[i]['CardsRemaining'])
-                              for i in range(len(games_to_farm))])
-        appids = [games_to_farm[i]['AppID'] for i in range(len(games_to_farm))]
+            games_to_farm = get(
+                f'Bot/{bot}').json()['Result'][bot]['CardsFarmer']['GamesToFarm']
+            hours_played = max([games_to_farm[i]['HoursPlayed']
+                            for i in range(len(games_to_farm))], default=0)
+            remaining_cards = sum([int(games_to_farm[i]['CardsRemaining'])
+                                for i in range(len(games_to_farm))])
+            appids = [games_to_farm[i]['AppID'] for i in range(len(games_to_farm))]
 
-        if(len(appids) != 0):
-            if(hours_played >= 3):
-                log(f'Se encontraron {len(appids)} juegos para farmear, {str(remaining_cards)} cromos restantes')
-                log(f'Farmeando {len(appids[:32])} juegos por 5 minutos')
-                cmd(f'play {bot} {",".join(list(map(str,appids[:32])))}')
-                sleep(300)
-            else:
-                log(f'Ningun juego llegó a las 3 horas aún')
-                remaining_time_hours = 3 - hours_played
-                remaining_time_minutes = remaining_time_hours * 60
-                remaining_time_seconds = remaining_time_minutes * 60
-                if remaining_time_hours < 1:
-                    log(
-                        f'Farmeando {len(appids[:31])} juegos por {str(round(remaining_time_minutes))} minutos')
+            if(len(appids) != 0):
+                if(hours_played >= 3):
+                    log(f'{bot} - Se encontraron {len(appids)} juegos para farmear, {str(remaining_cards)} cromos restantes')
+                    log(f'{bot} - Farmeando {len(appids[:32])} juegos por 5 minutos')
+                    cmd(f'play {bot} {",".join(list(map(str,appids[:32])))}')
+                    sleep(300)
                 else:
-                    log(
-                        f'Farmeando {len(appids[:31])} juegos por {str(round(remaining_time_hours, 1))} horas')
-                cmd(f'play {bot} {",".join(list(map(str,appids[:32])))}')
-                sleep(remaining_time_seconds)
-        else:
-            log(f'No hay juegos para farmear en {bot}')
-            break
+                    log(f'{bot} - Ningun juego llegó a las 3 horas aún')
+                    remaining_time_hours = 3 - hours_played
+                    remaining_time_minutes = remaining_time_hours * 60
+                    remaining_time_seconds = remaining_time_minutes * 60
+                    if remaining_time_hours < 1:
+                        log(
+                            f'{bot} - Farmeando {len(appids[:31])} juegos por {str(round(remaining_time_minutes))} minutos')
+                    else:
+                        log(
+                            f'{bot} - Farmeando {len(appids[:31])} juegos por {str(round(remaining_time_hours, 1))} horas')
+                    cmd(f'play {bot} {",".join(list(map(str,appids[:32])))}')
+                    sleep(remaining_time_seconds)
+            else:
+                log(f'{bot} - No hay juegos para farmear')
+                break
 
-        log('Pausando por 20 segundos')
-        cmd(f'pause {bot}')
-        sleep(20)
+            log(f'{bot} - Pausando por 20 segundos')
+            cmd(f'pause {bot}')
+            sleep(20)
 
-        for appid in appids:
-            log(f'Farmeando {appid} por 7 segundos...')
-            cmd(f'play {bot} {appid}')
-            sleep(7)
+            for appid in appids:
+                log(f'{bot} - Farmeando {appid} por 7 segundos...')
+                cmd(f'play {bot} {appid}')
+                sleep(7)
 
-        if(ismain == False):
-            cmd(f'loot {bot}')
+            if(ismain == False):
+                cmd(f'loot {bot}')
+    except:
+        log(f'{bot} - Desconectado')
+
 
 
 def wait_for_threads(threads: list[threading.Thread]):
